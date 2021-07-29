68c3f61b0f73c3a5ccfbd35b00288bdc62918abf
Natalia_Butramenka_Step_21 <nyfira0ku0kish@gmail.com>
2021-07-29 03:25:47 +0300

    numbers

---
+++ index.js
@@ -0,0 +1,8 @@
+let number1 = Number(prompt('Введите число'));
+if (number1>0) {
+    alert('Положительное число')
+} else if (number1===0) {
+    alert(0)
+} else if (number1<0) {
+    alert('Отрицательное число')
+}

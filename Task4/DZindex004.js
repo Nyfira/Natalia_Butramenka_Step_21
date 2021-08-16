91f196b7dfa41ce6e6e0d1f309be266e2cccfaec
Nyfira <nyfira0ku0kish@gmail.com>
2021-08-16 15:43:36 +0300

    dz 4 start

---
+++ Task4/DZindex004.js
@@ -0,0 +1,52 @@
+//  Task 1
+//function num(a,b) {
+//    let x = (a<b) ? `-1`:
+//    (a>b) ? `1`:
+//    (a==b) ? `0`:
+//   (`опаньки`);
+//    return (x);
+//};
+//console.log(num(154,154));
+
+//  Task 2
+//function fak(a) {
+//let x = 1;
+//while (a > 0) {
+//    x = x * a;
+//    a--  
+//    };
+//};
+//console.log(fak(5));
+
+//alert(`${x}`);
+
+
+
+// Задача NN2
+//let a = +prompt(`start`);
+//let result = 1;
+// while ( a > 0 ) {
+//    result = result * a;
+//   a--;
+//};
+//document.write(`${result}`); 
+
+
+//  Task 3
+//function num3(a,b,c) {
+//    let x = (`${a}${b}${c}`);
+//    return (x);
+//};
+//console.log(num3(2,5,1));
+
+// Task 4
+//function square(a,b=0) {
+//if (b == 0) {
+//    s = a*a;
+//} else {
+//    s = a*b;
+//};
+//return (s);
+//};
+//console.log(square(2));
+    

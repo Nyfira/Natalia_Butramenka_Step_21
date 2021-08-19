0e023b65acb8fe9297a978c08460d8603fa71249
Nyfira <nyfira0ku0kish@gmail.com>
2021-08-19 21:29:33 +0300

    massif

--- Taskjs5/index005.js
+++ Taskjs5/index005.js
@@ -228,14 +228,126 @@
 //pryamoy(koord)
 
 
-///dz 1
+///dz 1 !!!
+//let auto = {
+//    prozvoditel : `Volkswagen`,
+//    model : `Polo`,
+//    age : 2015,
+//    speed : 10,
+//};
+//function automobil () {
+//  return alert(auto[key])
+//};
 
-let auto = {
-    prozvoditel : `Volkswagen`,
-    model : `Polo`,
-    age : `2015`,
-    speed : `1500`
+//let key = prompt(`Что Вы хотите узнать об автомобиле?`);
+//automobil();
+
+//function time () {
+//    let t = mile/(auto.speed);
+//    
+//  return alert(`На дорогу Вы потратите ${t} часов.`)
+//};
+//let mile = prompt(`Какое расстояние Вам необходимо проехать?`);
+//time();
+
+///dz 2
+//let auto = {
+//    prozvoditel : `Volkswagen`,
+//    model : `Polo`,
+//    age : 2015,
+//    speed : 10,
+//};
+
+
+// massif
+// Task 1
+// 1
+//let arr = [9, 1, 10, 4, 5, 25, 13, 115, 200, 14 ];
+
+//function arrayOnDis(array) {
+//    document.write(array);
+//};
+//arrayOnDis(arr);
+
+//  2
+//let arr = [9, 1, 10, 4, 5, 25, 13, 115, 205, 14 ];
+//console.log(arr.length);
+
+//function arrayOnDis(array) {
+//  for (let i = 0; i < array.length; i++) {
+
+//      if (array[i] % 2 === 0) {
+//      document.write(`${array[i]}, `);
+//      }; 
+//  };
+//};
+//arrayOnDis(arr);
+
+// 3
+//let arr = [9, 1, 10, 4, 5, 25, 13, 115, 200, 14 ];
+
+//function arraySumm(array) {
+//    let sum = 0;
+//    for (let i = 0; i < array.length; i++) {
+//          sum = sum+array[i];
+//    }; 
+//    document.write(`${sum} `);
+//};
+//arraySumm(arr);
+
+// 4
+
+//function arrayBig(array) {
+//    let big = 0;
+//        for (let i = 0; i < array.length; i++) {
+//            if (big < array[i]) {
+//                big = array[i];
+//            }; 
+//        };
+//document.write (big);
+//};
+//arrayBig(arr);
+    
+// 5 
+
+//function arrayPluss(elem,index) {
+//    arr[index] = elem;
+//    document.write(arr);
+//};
+//arrayPluss(14,15);
+
+// 6
+
+//function arrayMinus(elem,index) {
+//    arr[index] = elem;
+//    document.write(arr);
+//};
+//arrayMinus(``,8);
+
+//  или
+
+//function arrayMinus(array) {
+//    delete arr[9];
+//    document.write(arr);
+//};
+//arrayMinus();
+
+// или
+
+//function arrayMinus(array, index) {
+//    array.splice(index,1);
+//    document.write(array);
+//};
+//arrayMinus(arr, 8);
+
+// Task 2
+// 1
+let arr1 = [ 13, 115, 200, 14, 1 ];
+let arr2 = [ 10, 11, 15, 14, 8 ];
+
+function Summ(arr1, arr2) {
+    let newarr= arr1.concat(arr2);
+    let newarr0
+       document.write(newarr0); 
 };
-function automobil () {
-alert(`auto`)
-};
+Summ(newarr0);

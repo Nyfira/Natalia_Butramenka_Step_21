7d6cde3677509645d39ac46b914c46b6f40db274
Nyfira <nyfira0ku0kish@gmail.com>
2021-09-20 21:01:54 +0300

    js 11 lesson

---
+++ Taskjs11/index011.js
@@ -0,0 +1,61 @@
+// Task 1
+// let block = document.querySelector('#block');
+// let btn = document.querySelector('#btn');
+
+// let num = 0;
+ 
+// function incre(event) {
+//     console.log(event.target.id); 
+//     if (event.target.id === 'incr') {
+//         num = ++num;
+//     } else {
+//         num = --num;
+//     }
+//     block.innerHTML=num;
+// };
+
+// btn.addEventListener('click', incre);
+
+// Task 2
+
+// let block = document.querySelector('#block');
+// let btn = document.querySelector('#btn');
+
+// let num = 0;
+
+// function addB() {
+//     const randomColor = Math.floor(Math.random()*16777215).toString(16);
+//     let el = document.createElement('div');
+//     el.style.width = '100px';
+//     el.style.height = '60px';
+//     el.style.backgroundColor = '#' + randomColor;
+// el.id='id'+ num;
+// num++;
+// block.append(el);
+// el.addEventListener('click', deleteBlock);
+// };
+
+// function deleteBlock(event) {
+//     const elid = event.target.id;
+//     let el = document.querySelector(`#${elid}`);
+//     el.style.display = 'none';
+// };
+
+// btn.addEventListener('click', addB);
+
+// Task 3
+
+// let text = document.querySelector('#text ');
+// let btn = document.querySelector('#btn');
+
+// function findColor(event) {
+//     const colid = event.target.id;
+//     text.style.color = colid;
+// };
+   
+// btn.addEventListener('click', findColor);
+
+// Task 4
+
+
+

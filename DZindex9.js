71512fea17c1529ccc4159f3a23bd763ff60dbe4
Nyfira <nyfira0ku0kish@gmail.com>
2021-09-21 14:39:43 +0300

    leson js 9

---
+++ DZindex9.js
@@ -0,0 +1,16 @@
+// Task 1
+class Marker {
+    constructor(color, ink) {
+        this.color = color;
+        this.ink = ink;
+    };
+
+    SetText(text) {
+
+    }
+}
+
+let string1 = new Marker(`red`, `100%`);
+string1.SetText();
+
+document.write(string1.SetText());
